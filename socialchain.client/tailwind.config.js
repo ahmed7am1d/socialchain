674c9b35e5d8a27998cde0b0c09d61c74c28a5a7
ahmed7am1d <a_al_doori@utb.cz>
2023-03-19 17:18:36 +0100

    configs update

--- socialchain.client/tailwind.config.js
+++ socialchain.client/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
+    "./layouts/**/*.{js,ts,jsx,tsx}",
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -13,8 +13,9 @@ module.exports = {
       colors: {
         darkBlack: "#1C1E21",
         darkBlue:"#1E2B3D",
+        darkBlueHalfTrans:"#1E2B3D80",
         primaryPinkColor: "#F213A4",
-        primaryPinkColorTrans: "#f213a457",
+        primaryPinkColorTrans: "#f213a499",
         secondaryGold: "#BC9F4F",
         grayTextColor:"#b5b3b3c3"
       },
